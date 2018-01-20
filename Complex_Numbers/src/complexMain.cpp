4713b158b61ecc3553db91304a6d4b3d403afe31
broflowsky <v.puyfourcat@gmail.com>
2018-01-20 12:39:36 -0500

    figured it out

---
+++ Complex_Numbers/src/complexMain.cpp
@@ -0,0 +1,87 @@
+//gitHub sucks AAAAAAAAAAAASSSSSSSSSSSSSSSSSSSSSS
+#include "Complex.h"
+#include <iostream>
+#include <istream>
+#include <fstream>
+#include <list>
+
+#define VECTOR true
+#define CARTESIAN false
+#define outFile "complexData.txt"
+
+using namespace std;
+
+void eraseData(ofstream&,string);
+bool initStreams(ifstream&, ofstream&, string file);
+int main(){
+
+	ifstream in;
+	ofstream out;
+	bool quit = false;
+	/*eraseData(out,outFile);*/
+
+	if(initStreams(in,out,outFile)){
+		cerr<<"Stream error";
+		return 1;
+	}
+	while(!quit){
+		cout<<"\nEnter complex number:\n";
+		string *input = new string;
+		getline(cin,*input);
+		cin.clear();
+		Complex *c = new Complex(*input);
+		c->update(VECTOR);
+		c->print(VECTOR);
+		delete input;
+		delete c;
+	}
+	Complex *c = new Complex("0.5 j-0.866");
+	//c->update(CARTESIAN);
+	//c->print(CARTESIAN);
+	cout<<'\n';
+	c->update(VECTOR);
+	c->print(VECTOR);
+
+	out.close();
+	//eraseData(out,outFile);
+
+	return 0;
+}
+bool initStreams(ifstream &in, ofstream& out, string File){
+	return (!in && !out);
+}
+void eraseData(ofstream &out,string File){
+
+	if(out)
+		out.close();
+	out.open(File);
+	out<<' ';
+	out.close();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
